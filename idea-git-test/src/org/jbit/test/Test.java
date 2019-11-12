968f85e5fcb43bf79f037ead24f2dd6ae78be3d4
KuaQ <xzqminiq@qq.com>
2019-11-12 09:08:06 +0800

    测试实体类

--- idea-git-test/src/org/jbit/test/Test.java
+++ idea-git-test/src/org/jbit/test/Test.java
@@ -1,4 +1,13 @@
 package org.jbit.test;
 
 public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+
+    }
+
 }
